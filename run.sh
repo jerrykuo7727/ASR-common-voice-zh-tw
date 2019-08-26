9a734a5b339b3480c94fb40f3374b4449609f759
Jiazhi Guo <jerrykuo7727@gmail.com>
2019-08-26 16:38:39 +0800

    Modify to train and score triphone model.

--- run.sh
+++ run.sh
@@ -107,3 +107,28 @@ utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
 steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
                 exp/mono/graph data/test exp/mono/decode
 local/score.sh data/test data/lang exp/mono/decode/
+
+echo
+echo "===== MONO ALIGNMENT ====="
+echo
+
+steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali || exit 1
+
+echo
+echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo
+
+steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1
+
+echo
+echo "===== TRI1 (first triphone pass) DECODING ====="
+echo
+
+utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
+                exp/tri1/graph data/test exp/tri1/decode
+local/score.sh data/test data/lang exp/tri1/decode
+
+echo
+echo "===== run.sh script is finished ====="
+echo
