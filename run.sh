a0abbd58977e9c28518d2ac93e5a13f817210ede
Jiazhi Guo <jerrykuo7727@gmail.com>
2019-09-17 16:48:24 +0800

    Refine run.sh for better readability.

--- run.sh
+++ run.sh
@@ -12,58 +12,49 @@ lm_order=3
 . utils/parse_options.sh || exit 1
 [[ $# -ge 1 ]] && { echo "Wrong arguments!"; exit 1; }
 
-
-# Removing previously created data (from last run.sh execution)
-rm -rf data exp mfcc   
-
+rm -rf data exp mfcc
 
 echo
-echo "===== PREPARING REQUIRED DATA ====="
+echo "---- Prepare basic data from CommonVoice_zh_TW ----"
 echo
 
 mkdir data data/train data/test data/local data/local/dict
 $python3_cmd scripts/prepare_data.py
 
 echo
-echo "===== PREPARING ACOUSTIC DATA ====="
+echo "---- Prepare acoustic data ----"
 echo
 
-# Making spk2utt files
+# Reverse utt2spk to spk2utt
 utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
 
 echo
-echo "===== FEATURES EXTRACTION ====="
+echo "---- Feature extraction ----"
 echo
 
-# Making feats.scp files
+# MFCC
 mfccdir=mfcc
-# Uncomment and modify arguments in scripts below if you have any problems with data sorting
-# utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
-# utils/validate_data_dir.sh data/test
-# utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
-# utils/fix_data_dir.sh data/test
-
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train \
                         exp/make_mfcc/train $mfccdir
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/test \
                         exp/make_mfcc/test $mfccdir
 
-# Making cmvn.scp files
+# CMVN
 steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
 steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
 
 echo
-echo "===== PREPARING LANGUAGE DATA ====="
+echo "---- Prepare language data ----"
 echo
-# Preparing language data
+
 utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
 
 echo
-echo "===== LANGUAGE MODEL CREATION ====="
-echo "===== MAKING lm.arpa ====="
+echo "---- Create language model ----"
 echo
 
+# Check if SRILM was installed
 loc=`which ngram-count`;
 if [ -z $loc ]; then
     if uname -a | grep 64 >/dev/null; then
@@ -80,27 +71,24 @@ if [ -z $loc ]; then
     fi
 fi
 
+# Train language model
 local=data/local
 mkdir $local/tmp
 ngram-count -order $lm_order -write-vocab $local/tmp/vocab-full.txt \
             -wbdiscount -text $local/corpus.txt -lm $local/tmp/lm.arpa
 
-echo
-echo "===== MAKING G.fst ====="
-echo
-
+# Make G.FST
 lang=data/lang
-arpa2fst --disambig-symbol=#0 --read-symbol-table=$lang/words.txt \
-                          $local/tmp/lm.arpa $lang/G.fst
+arpa2fst --disambig-symbol=$local/tmp/lm.arpa $lang/G.fst
 
 echo
-echo "===== MONO TRAINING ====="
+echo "---- Monophone | Training ----"
 echo
 
 steps/train_mono.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono  || exit 1
 
 echo
-echo "===== MONO DECODING ====="
+echo "---- Monophone | Decoding ----"
 echo
 
 utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
@@ -109,19 +97,19 @@ steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
 local/score.sh data/test data/lang exp/mono/decode/
 
 echo
-echo "===== MONO ALIGNMENT ====="
+echo "---- Monophone | Alignment ----"
 echo
 
 steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali || exit 1
 
 echo
-echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo "---- Triphone (1st): Delta + Delta-Delta | Training ----"
 echo
 
 steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1
 
 echo
-echo "===== TRI1 (first triphone pass) DECODING ====="
+echo "---- Triphone (1st): Delta + Delta-Delta | Decoding ----"
 echo
 
 utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
@@ -130,5 +118,27 @@ steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" \
 local/score.sh data/test data/lang exp/tri1/decode
 
 echo
-echo "===== run.sh script is finished ====="
+echo "---- Triphone (1st): Delta + Delta-Delta | Alignment ----"
+echo
+
+steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_ali || exit 1
+
+echo 
+echo "---- Triphone (2nd): LDA + MLLT | Training ----"
+echo 
+
+steps/train_lda_mllt.sh --cmd "$train_cmd" \
+    --splice-opts "--left-context=3 --right-context=3" \
+    2000 11000 data/train data/lang exp/tri1_ali exp/tri2
+
+echo 
+echo "---- Triphone (2nd): LDA + MLLT | Decoding ----"
+echo 
+
+utils/mkgraph.sh data/lang exp/tri2 exp/tri2/graph || exit 1
+steps/decode.sh --nj $nj --cmd "$decode_cmd" exp/tri2/graph data/test exp/tri2/decode
+local/score.sh data/test data/lang exp/tri2/decode
+
+echo
+echo "---- Script ended successfully ----"
 echo
